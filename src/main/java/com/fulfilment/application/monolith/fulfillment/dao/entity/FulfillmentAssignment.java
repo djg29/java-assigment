60291b9aa39abd428349bb33f3f2be11546cc2c2
djg29 <gogoidhrub@gmail.com>
2026-06-04 16:51:02 +0200

    test fix

--- src/main/java/com/fulfilment/application/monolith/fulfillment/dao/entity/FulfillmentAssignment.java
+++ src/main/java/com/fulfilment/application/monolith/fulfillment/dao/entity/FulfillmentAssignment.java
@@ -1,12 +1,10 @@
 package com.fulfilment.application.monolith.fulfillment.dao.entity;
 
 import com.fulfilment.application.monolith.fulfillment.model.Fulfillment;
-import jakarta.persistence.Cacheable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import java.math.BigDecimal;
 
 @Entity
 public class FulfillmentAssignment {
