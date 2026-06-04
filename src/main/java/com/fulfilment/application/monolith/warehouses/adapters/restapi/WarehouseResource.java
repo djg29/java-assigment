585e3bffed2447318f122a7ae3943579879d428f
djg29 <gogoidhrub@gmail.com>
2026-06-04 15:18:17 +0200

    fix valid

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseResource.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseResource.java
@@ -1,19 +1,19 @@
 package com.fulfilment.application.monolith.warehouses.adapters.restapi;
 
 import com.fulfilment.application.monolith.warehouses.domain.models.Warehouse;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.Valid;
 import java.util.List;
 
 public interface WarehouseResource {
 
     public List<Warehouse> listAllWarehousesUnits();
 
-    public Warehouse createANewWarehouseUnit(@NotNull Warehouse data);
+    public Warehouse createANewWarehouseUnit(@Valid Warehouse data);
 
     public Warehouse getAWarehouseUnitByID(String id);
 
     public void archiveAWarehouseUnitByID(String id);
 
     public Warehouse replaceTheCurrentActiveWarehouse(
-            String businessUnitCode, @NotNull Warehouse data);
+            String businessUnitCode, @Valid Warehouse data);
 }
